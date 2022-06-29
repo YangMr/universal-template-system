2ba55266efbef4a45a01ce9d9f0d92a8f461dd58
YangMr <759168442@qq.com>
2022-06-29 09:48:25 +0800

    feat(menu): 动态渲染菜单

--- src/layout/Sidebar/index.vue
+++ src/layout/Sidebar/index.vue
@@ -1,88 +1,114 @@
 <template>
   <div>
-    <menu-tree :tree="data"></menu-tree>
+     <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="/profile"
+        text-color="#fff"
+        router
+      >
+        <!-- 如果菜单数据里面没有子菜单数据,则使用el-menu-item渲染
+        如果菜单数据里面有子菜单数据,则使用el-sub-menu渲染 -->
+        <!-- <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+
+          <el-menu-item index="1-3">item three</el-menu-item>
+
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu> -->
+        <template v-for="(item,index) in menuList" :key="item">
+          <template v-if="item && !item.children">
+            <el-menu-item :index="item.path">
+              <el-icon><icon-menu /></el-icon>
+              <span>{{item.title}}</span>
+            </el-menu-item>
+          </template>
+
+          <template v-if="item && item.children && item.children.length > 0">
+            <el-sub-menu :index="index">
+               <template #title>
+                  <el-icon><location /></el-icon>
+                  <span>{{item.title}}</span>
+               </template>
+
+              <template v-for="(m) in item.children" :key="m">
+                  <template v-if="m && !m.children">
+                    <el-menu-item :index="m.path">
+                      {{m.title}}
+                    </el-menu-item>
+                  </template>
+                  <template v-if="m && m.children && m.children.length > 0">
+                    <el-sub-menu :index="m">
+                      <template #title>
+                          <el-icon><location /></el-icon>
+                          <span>{{m.title}}</span>
+                      </template>
+                    </el-sub-menu>
+                  </template>
+              </template>
+
+            </el-sub-menu>
+          </template>
+
+        </template>
+
+      </el-menu>
   </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
-import MenuTree from './MenuTree.vue'
-
-const array = [
+const menuList = reactive([
   {
-    id: 1,
-    name: 'IT互联网',
+    title: '个人中心',
+    path: '/profile'
+  },
+  {
+    title: '用户',
     children: [
       {
-        id: 11,
-        name: '前端',
+        title: '员工管理',
+        path: '/staff',
         children: [
           {
-            id: 111,
-            name: 'html',
-            children: [
-              {
-                id: 11113,
-                name: 'p'
-              },
-              {
-                id: 11114,
-                name: 'h1'
-              },
-              {
-                id: 11115,
-                name: 'ul'
-              }
-            ]
+            title: '市场员工'
           },
           {
-            id: 112,
-            name: 'css'
-          },
-          {
-            id: 113,
-            name: 'javascript'
+            title: '研发员工'
           }
         ]
       },
       {
-        id: 12,
-        name: '后端'
+        title: '角色列表',
+        path: '/role'
       },
       {
-        id: 13,
-        name: '运维'
+        title: '权限列表',
+        path: '/premission'
       }
     ]
   },
   {
-    id: 2,
-    name: '设计创作',
+    title: '文章',
     children: [
       {
-        id: 22,
-        name: '平面设计'
+        title: '文章排名',
+        path: '/article'
       },
       {
-        id: 23,
-        name: '网页设计'
+        title: '创建文章',
+        path: '/create'
       }
     ]
-  },
-  {
-    id: 3,
-    name: '升学考研',
-    chidlren: []
-  },
-  {
-    id: 1,
-    name: '职企考证',
-    chidlren: []
   }
-]
-
-const data = reactive(array)
-
+])
 </script>
 
 <style lang="scss" scoped>
