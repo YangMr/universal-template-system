58a7c1764ce4d24794b6704e3cd2f77d590b485f
YangMr <759168442@qq.com>
2022-07-01 11:45:36 +0800

    feat(permission): 给动态添加路由代码加注释

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -14,16 +14,20 @@ export default {
      * 根据权限筛选路由
      */
     filterRoutes({ commit }, menus) {
+      // 当用户登录之后是加载所有的私有路由表还是加载他所拥有的私有表
+      // menus 登录之后后台所返回的用户所有的路由权限数据
+      // privateRoutes 自定义的所有私有路由表
       const routes = []
+      // 过滤出用户所拥有的私有路由表
       menus.forEach(name => {
         const data = privateRoutes.filter(item => item.name === name)
         routes.push(...data)
       })
+      // 添加 重定向 404 路由
       routes.push({
         path: '/:catchAll(.*)',
         redirect: '/404'
       })
-      console.log('routes====>', routes)
       commit('setRoutes', routes)
       return routes
     }
