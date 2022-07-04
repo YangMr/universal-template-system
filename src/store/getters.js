e62f0d9b0f4173fe62f77680b95b29c88afc18c1
YangMr <759168442@qq.com>
2022-07-04 10:53:54 +0800

    feat(tagsview): 实现tagsview功能

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,8 @@ const getters = {
   userInfo: state => state.user.userInfo,
   hasUserInfo: state => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  tagsview: state => state.tagsview.tagsView
 }
 
 export default getters
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
 import user from './modules/user'
 import permission from './modules/permission'
+import tagsview from './modules/tagsview'
 import getters from './getters'
 export default createStore({
   getters,
   modules: {
     user,
-    permission
+    permission,
+    tagsview
   }
 })
